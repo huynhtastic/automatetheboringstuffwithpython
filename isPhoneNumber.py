8e050e7f109f9b1c3363229d702da3c46adda762
Richard Huynh <huynhtastic@gmail.com>
2017-05-09 22:17:26 -0500

    initial commit; finished first revision of isPhoneNumber.py

---
+++ isPhoneNumber.py
@@ -0,0 +1,22 @@
+def isPhoneNumber(text):
+  if len(text) != 12: # not a phone number
+    return False
+  for i in range(0, 3): # check for area code
+    if not text[i].isdecimal():
+      return False
+  if text[3] != '-': # check for hypen
+    return False
+  for i in range(4, 7): # check for first 3 numbers
+    if not text[i].isdecimal():
+      return False
+  if text[7] != '-': # check for hypen
+    return False
+  for i in range(8, 12): # check for last 4 numbers
+    if not text[i].isdecimal():
+      return False
+  return True
+
+print('415-555-4242 is a phone number:')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi is a phone number:')
+print(isPhoneNumber('Moshi moshi'))
